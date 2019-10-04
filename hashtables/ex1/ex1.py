a254b17a35ef7c2737a98a4bf3e9a15a60e8e926
Lidii <lidiiany92@gmail.com>
2019-10-04 08:54:43 -0700

    Ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,11 +9,22 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    indexes = []
 
-    return None
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+
+    for j in range(length):
+        result = hash_table_retrieve(ht, limit-weights[j])
+        if result != None:
+            indexes.append(result)
+
+    if indexes and indexes[0] != indexes[1]:
+        return indexes
+    elif indexes and indexes[0] == indexes[1]:
+        return [indexes[1], 0]
+    else: 
+        return None
 
 
 def print_answer(answer):
